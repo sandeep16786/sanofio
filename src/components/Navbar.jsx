a357256dd097563311ebdb26ac26f868b4f84f08
sandeep16786 <sandeepdodiya1@gmail.com>
2024-08-14 12:49:41 +0530

    update 1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
         <>
             <nav className={`navbar ${isSticky ? 'sticky' : ''} navbar-expand-lg bg-body-tertiary bg-white header header-menu-wrapper main-menu`}>
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#"><img src="src/assets/img/logo-black.svg" alt="logo" /></a>
+                    <a className="navbar-brand" href="#"><img src="../../src/assets/img/logo-black.svg" alt="logo" /></a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
